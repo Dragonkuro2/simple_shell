547cc92adf5793f02502f806447cf05df9061847
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-10-06 12:31:48 -0700

    I added the prototypes of the function that I added

--- main.h
+++ main.h
@@ -12,6 +12,19 @@
 
 char *get_linefunc(void);
 char **splitter(char *line);
-
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _atoi(char *s);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
 
 #endif
