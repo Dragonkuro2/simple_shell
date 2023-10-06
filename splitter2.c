2daf0881bc7bc09ac4743524acbaefd413ee3c1a
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-10-06 04:33:58 -0700

    the version 2 of the function splitter that you did, i make smaller now

---
+++ splitter2.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * **splitter - splits the line according to the delemitrs
+ * @line : string to split
+ * Return: the splited string
+ */
+char **splitter(char *line)
+{
+	char *split, **splits = NULL;
+	int counter = 0, i = 0;
+
+	if (line == NULL)
+		return (NULL);
+
+	split = strtok(line, " \t\n");
+	while (split)
+	{
+		counter++;
+		splits = realloc(splits, counter * sizeof(char *));
+		if (!splits)
+		{
+			for (; i < counter - 1; i++)
+				free(splits[i]);
+			free(splits);
+			return (NULL);
+		}
+		splits[counter - 1] = strdup(split);
+		if (!splits[counter - 1])
+		{
+			for (i = 0; i < counter - 1; i++)
+                                free(splits[i]);
+                        free(splits);
+                        return (NULL);
+                }
+		split = strtok(NULL, " \n\t");
+	}
+	return (splits);
+}
